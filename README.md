90fac6159754c6e977ed546743026fbf0850ae8e
Panagiotis Liaskos <pnliaskos@gmail.com>
2023-03-28 12:44:38 +0300

    Added Images, Text to FireBase Cloud

---
+++ README.md
@@ -0,0 +1,11 @@
+# Unipi Audio Stories | Mobile Device Application Development final project
+
+## A text-to-speech fairy tale reading application.
+
+<hr>
+<br>
+Collaborators: <a href="https://github.com/pnliaskos"> Panagiotis-Nikolaos Liaskos </a>
+<br>
+Technologies: Java, Firebase, Android Studio 
+<br>
+Status: in progress
---
+++ app/src/main/java/unipi/projects/unipiaudiostories/MainActivity2.java
@@ -0,0 +1,182 @@
+package unipi.projects.unipiaudiostories;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView messageOutput;
+    ImageView downloadedPicture;
+    FirebaseDatabase database;
+    DatabaseReference reference1, reference2, reference3, reference4, reference5;
+
+    StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        messageOutput = findViewById(R.id.textView);
+        database = FirebaseDatabase.getInstance();
+        reference1 = database.getReference("message1");
+        reference2 = database.getReference("message2");
+        reference3 = database.getReference("message3");
+        reference4 = database.getReference("message4");
+        reference5 = database.getReference("message5");
+        downloadedPicture = findViewById(R.id.imageView);
+        storageReference = FirebaseStorage.getInstance().getReference();
+    }
+
+    public void story1 (View view) {
+        reference1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messageOutput.setText(snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                messageOutput.setText(error.getMessage());
+            }
+        });
+
+        StorageReference imageRef1 = storageReference.child("king.jpg");
+        try {
+            File localFile = File.createTempFile("tempImage", "jpg");
+            imageRef1.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    downloadedPicture.setImageBitmap(BitmapFactory.decodeFile(localFile.getAbsolutePath()));
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void story2 (View view) {
+        reference2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messageOutput.setText(snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                messageOutput.setText(error.getMessage());
+            }
+        });
+
+        StorageReference imageRef2 = storageReference.child("pot.jpg");
+        try {
+            File localFile = File.createTempFile("tempImage", "jpg");
+            imageRef2.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    downloadedPicture.setImageBitmap(BitmapFactory.decodeFile(localFile.getAbsolutePath()));
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void story3 (View view) {
+        reference3.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messageOutput.setText(snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                messageOutput.setText(error.getMessage());
+            }
+        });
+
+        StorageReference imageRef3 = storageReference.child("tosodoulis.jpg");
+        try {
+            File localFile = File.createTempFile("tempImage", "jpg");
+            imageRef3.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    downloadedPicture.setImageBitmap(BitmapFactory.decodeFile(localFile.getAbsolutePath()));
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void story4 (View view) {
+        reference4.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messageOutput.setText(snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                messageOutput.setText(error.getMessage());
+            }
+        });
+
+        StorageReference imageRef4 = storageReference.child("fox.jpg");
+        try {
+            File localFile = File.createTempFile("tempImage", "jpg");
+            imageRef4.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    downloadedPicture.setImageBitmap(BitmapFactory.decodeFile(localFile.getAbsolutePath()));
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void story5 (View view) {
+        reference5.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messageOutput.setText(snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                messageOutput.setText(error.getMessage());
+            }
+        });
+
+        StorageReference imageRef5 = storageReference.child("pigs.jpg");
+        try {
+            File localFile = File.createTempFile("tempImage", "jpg");
+            imageRef5.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    downloadedPicture.setImageBitmap(BitmapFactory.decodeFile(localFile.getAbsolutePath()));
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
